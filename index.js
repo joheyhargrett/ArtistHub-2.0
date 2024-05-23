5d84fc6797414e28146163153addefe94a556dd6
Johey Hargrett <joheyhargrett@gmail.com>
2024-05-23 12:31:17 -0400

    added more artists on my display

--- index.js
+++ index.js
@@ -4,112 +4,119 @@ const artistData = [
   { id: "790FomKkXshlbRYZFtlgla", name: "Karol G" },
   { id: "12Chz98pHFMPJEknJQMWvI", name: "Muse" },
   { id: "181bsRPaVXVlUKXrxwZfHK", name: "Megan Thee Stallion" },
+  { id: "3TVXtAsR1Inumwj472S9r4", name: "Drake" },
+  { id: "1Xyo4u8uXC1ZmMpatF05PJ", name: "The Weeknd" },
+  { id: "53XhwfbYqKCa1cC15pYq2q", name: "Imagine Dragons" },
+  { id: "1HY2Jd0NmPuamShAr6KMms", name: "Lady Gaga" },
+  { id: "5pKCCKE2ajJHZ9KAiaK11H", name: "Rihanna" }
 ];
 
 let data = {};
 
 const requestToken = async () => {
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: "grant_type=client_credentials&client_id=64ffb24515d44051b073917a2bd60326&client_secret=7ba47ffd43fc4c799c3e9a64b02b2456",
-  });
+  try {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: "grant_type=client_credentials&client_id=64ffb24515d44051b073917a2bd60326&client_secret=7ba47ffd43fc4c799c3e9a64b02b2456",
+    });
 
-  data = await response.json();
-  console.log(data);
+    if (!response.ok) {
+      throw new Error(`Token request failed with status ${response.status}`);
+    }
+
+    data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching token:', error);
+    alert('Failed to fetch token. Please try again later.');
+  }
 };
 
 const renderPage = async () => {
   await requestToken();
 
-  artistData.forEach((artist) => {
-    fetchArtist(artist);
+  artistData.forEach(async (artist) => {
+    const artistInfo = await fetchArtist(artist.id);
+    if (artistInfo) {
+      renderArtist(artist, artistInfo);
+    }
   });
 
   const searchButton = document.getElementById("searchButton");
   searchButton.addEventListener("click", searchArtist);
 };
 
-const resetArtistContainer = (artistId) => {
-  const artistContainer = content.querySelector(`[data-artist="${artistId}"]`);
+const fetchArtist = async (artistId) => {
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/artists/${artistId}`,
+      {
+        headers: { Authorization: `Bearer ${data.access_token}` },
+      }
+    );
 
-  const heartIcon = artistContainer.querySelector(".heart-icon");
-  heartIcon.innerHTML = isArtistHearted(artistId) ? "❤️" : "🤍";
-  heartIcon.classList.remove("pulsate");
-  clearAlbumDropdown();
-  clearTrackList(artistContainer.querySelector(".track-list-container"));
-};
+    if (!response.ok) {
+      throw new Error(`Failed to fetch artist with status ${response.status}`);
+    }
 
-const toggleHeart = (artistId) => {
-  const artistContainer = content.querySelector(`[data-artist="${artistId}"]`);
-  const heartIcon = artistContainer.querySelector(".heart-icon");
-  const isLiked = isArtistHearted(artistId);
-
-  if (isLiked) {
-    updateHeartedArtists(artistId, false);
-    heartIcon.innerHTML = "🤍";
-    heartIcon.classList.remove("pulsate");
-  } else {
-    updateHeartedArtists(artistId, true);
-    heartIcon.innerHTML = "❤️";
-    heartIcon.classList.add("pulsate");
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching artist ${artistId}:`, error);
+    return null;
   }
 };
 
 const fetchArtistGenre = async (artistId, artistContainer) => {
-  const response = await fetch(
-    `https://api.spotify.com/v1/artists/${artistId}`,
-    {
-      headers: { Authorization: `Bearer ${data.access_token}` },
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/artists/${artistId}`,
+      {
+        headers: { Authorization: `Bearer ${data.access_token}` },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch genre with status ${response.status}`);
     }
-  );
 
-  const artistInfo = await response.json();
-  const genre = artistInfo.genres[0];
+    const artistInfo = await response.json();
+    const genre = artistInfo.genres[0];
 
-  const genresContainer = artistContainer.querySelector(".genres");
+    const genresContainer = artistContainer.querySelector(".genres");
 
-  if (genre) {
-    const formattedGenre = genre
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
-    const genreElement = document.createElement("span");
-    genreElement.textContent = formattedGenre;
+    if (genre) {
+      const formattedGenre = genre
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+      const genreElement = document.createElement("span");
+      genreElement.textContent = formattedGenre;
 
-    genresContainer.appendChild(genreElement);
-  } else {
-    genresContainer.textContent = "No genre available";
+      genresContainer.appendChild(genreElement);
+    } else {
+      genresContainer.textContent = "No genre available";
+    }
+
+    const socialLinks = document.createElement("div");
+    socialLinks.classList.add("social-links");
+    artistInfo.external_urls &&
+      Object.keys(artistInfo.external_urls).forEach((platform) => {
+        const link = document.createElement("a");
+        link.href = artistInfo.external_urls[platform];
+        link.target = "_blank";
+        link.textContent =
+          platform === "spotify" ? "Follow Artist on Spotify" : platform;
+        socialLinks.appendChild(link);
+      });
+
+    artistContainer.appendChild(socialLinks);
+  } catch (error) {
+    console.error(`Error fetching genre for artist ID ${artistId}:`, error);
   }
-
-  const socialLinks = document.createElement("div");
-  socialLinks.classList.add("social-links");
-  artistInfo.external_urls &&
-    Object.keys(artistInfo.external_urls).forEach((platform) => {
-      const link = document.createElement("a");
-      link.href = artistInfo.external_urls[platform];
-      link.target = "_blank";
-      link.textContent =
-        platform === "spotify" ? "Follow Artist on Spotify" : platform;
-      socialLinks.appendChild(link);
-    });
-
-  artistContainer.appendChild(socialLinks);
 };
 
-const fetchArtist = async (artist) => {
-  if (content.querySelector(`[data-artist="${artist.id}"]`)) {
-    return;
-  }
-
-  const response = await fetch(
-    `https://api.spotify.com/v1/artists/${artist.id}`,
-    {
-      headers: { Authorization: `Bearer ${data.access_token}` },
-    }
-  );
-
-  const artistInfo = await response.json();
+const renderArtist = (artist, artistInfo) => {
   const artistContainer = document.createElement("div");
   artistContainer.classList.add("artist-container");
 
@@ -152,36 +159,37 @@ const fetchArtist = async (artist) => {
 
   artistImage.addEventListener("click", () => showAlbumDropdown(artist.id));
 
-  await fetchArtistGenre(artist.id, artistContainer);
+  fetchArtistGenre(artist.id, artistContainer);
 };
 
 const showAlbumDropdown = async (artistId) => {
-  clearAlbumDropdown();
+  try {
+    clearAlbumDropdown();
 
-  const albumDropdown = document.createElement("select");
-  albumDropdown.id = `albumDropdown-${artistId}`;
-  albumDropdown.classList.add("album-dropdown");
+    const albumDropdown = document.createElement("select");
+    albumDropdown.id = `albumDropdown-${artistId}`;
+    albumDropdown.classList.add("album-dropdown");
 
-  const defaultOption = document.createElement("option");
-  defaultOption.textContent = "Select an Album";
-  albumDropdown.appendChild(defaultOption);
+    const defaultOption = document.createElement("option");
+    defaultOption.textContent = "Select an Album";
+    albumDropdown.appendChild(defaultOption);
 
-  const response = await fetch(
-    `https://api.spotify.com/v1/artists/${artistId}/albums?market=US`,
-    {
-      headers: { Authorization: `Bearer ${data.access_token}` },
+    const response = await fetch(
+      `https://api.spotify.com/v1/artists/${artistId}/albums?market=US`,
+      {
+        headers: { Authorization: `Bearer ${data.access_token}` },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch albums with status ${response.status}`);
     }
-  );
 
-  const artistAlbums = await response.json();
-  renderAlbumDropdown(artistAlbums.items, artistId, albumDropdown);
-};
-
-const clearAlbumDropdown = () => {
-  const albumDropdowns = content.querySelectorAll(
-    "select[id^='albumDropdown-']"
-  );
-  albumDropdowns.forEach((dropdown) => dropdown.remove());
+    const artistAlbums = await response.json();
+    renderAlbumDropdown(artistAlbums.items, artistId, albumDropdown);
+  } catch (error) {
+    console.error(`Error fetching albums for artist ID ${artistId}:`, error);
+  }
 };
 
 const renderAlbumDropdown = (albums, artistId, albumDropdown) => {
@@ -212,42 +220,43 @@ const renderAlbumDropdown = (albums, artistId, albumDropdown) => {
   artistContainer.appendChild(albumDropdown);
 };
 
-const clearTrackList = (trackListContainer) => {
-  const existingTrackList = trackListContainer.querySelector("ul");
-  if (existingTrackList) {
-    existingTrackList.remove();
-  }
-
-  const existingAlbumCover = trackListContainer.querySelector(".album-cover");
-  if (existingAlbumCover) {
-    existingAlbumCover.remove();
-  }
-};
-
 const fetchAlbumTracks = async (albumId, trackListContainer) => {
-  const response = await fetch(
-    `https://api.spotify.com/v1/albums/${albumId}/tracks`,
-    {
-      headers: { Authorization: `Bearer ${data.access_token}` },
-    }
-  );
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/albums/${albumId}/tracks`,
+      {
+        headers: { Authorization: `Bearer ${data.access_token}` },
+      }
+    );
 
-  const albumTracks = await response.json();
-  const albumDetails = await fetch(
-    `https://api.spotify.com/v1/albums/${albumId}`,
-    {
-      headers: { Authorization: `Bearer ${data.access_token}` },
+    if (!response.ok) {
+      throw new Error(`Failed to fetch tracks with status ${response.status}`);
     }
-  );
-  const albumInfo = await albumDetails.json();
 
-  renderTrackList(
-    albumTracks.items,
-    trackListContainer,
-    albumInfo.images[1].url,
-    albumInfo.release_date,
-    albumInfo.total_tracks
-  );
+    const albumTracks = await response.json();
+    const albumDetails = await fetch(
+      `https://api.spotify.com/v1/albums/${albumId}`,
+      {
+        headers: { Authorization: `Bearer ${data.access_token}` },
+      }
+    );
+
+    if (!albumDetails.ok) {
+      throw new Error(`Failed to fetch album details with status ${albumDetails.status}`);
+    }
+
+    const albumInfo = await albumDetails.json();
+
+    renderTrackList(
+      albumTracks.items,
+      trackListContainer,
+      albumInfo.images[1].url,
+      albumInfo.release_date,
+      albumInfo.total_tracks
+    );
+  } catch (error) {
+    console.error(`Error fetching tracks for album ID ${albumId}:`, error);
+  }
 };
 
 const renderTrackList = (
@@ -288,18 +297,26 @@ const suggestionsContainer = document.getElementById("suggestions");
 searchInput.addEventListener("input", async () => {
   const searchTerm = searchInput.value.trim();
   if (searchTerm) {
-    const response = await fetch(
-      `https://api.spotify.com/v1/search?q=${searchTerm}&type=artist&limit=5`,
-      {
-        headers: { Authorization: `Bearer ${data.access_token}` },
-      }
-    );
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${searchTerm}&type=artist&limit=5`,
+        {
+          headers: { Authorization: `Bearer ${data.access_token}` },
+        }
+      );
 
-    const searchResults = await response.json();
-    if (searchResults.artists && searchResults.artists.items.length > 0) {
-      displaySuggestions(searchResults.artists.items);
-    } else {
-      clearSuggestions();
+      if (!response.ok) {
+        throw new Error(`Failed to search artists with status ${response.status}`);
+      }
+
+      const searchResults = await response.json();
+      if (searchResults.artists && searchResults.artists.items.length > 0) {
+        displaySuggestions(searchResults.artists.items);
+      } else {
+        clearSuggestions();
+      }
+    } catch (error) {
+      console.error(`Error searching for artists:`, error);
     }
   } else {
     clearSuggestions();
@@ -324,27 +341,36 @@ const clearSuggestions = () => {
 const searchArtist = async () => {
   const searchTerm = searchInput.value.trim();
   if (searchTerm) {
-    const response = await fetch(
-      `https://api.spotify.com/v1/search?q=${searchTerm}&type=artist`,
-      {
-        headers: { Authorization: `Bearer ${data.access_token}` },
-      }
-    );
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${searchTerm}&type=artist`,
+        {
+          headers: { Authorization: `Bearer ${data.access_token}` },
+        }
+      );
 
-    const searchResults = await response.json();
-    if (searchResults.artists && searchResults.artists.items.length > 0) {
-      const artist = searchResults.artists.items[0];
-      fetchArtist({ id: artist.id, name: artist.name });
-      searchInput.value = "";
-      clearSuggestions();
-    } else {
-      alert("No artists found for the given search term.");
+      if (!response.ok) {
+        throw new Error(`Failed to search artists with status ${response.status}`);
+      }
+
+      const searchResults = await response.json();
+      if (searchResults.artists && searchResults.artists.items.length > 0) {
+        const artist = searchResults.artists.items[0];
+        const artistInfo = await fetchArtist(artist.id);
+        if (artistInfo) {
+          renderArtist(artist, artistInfo);
+          searchInput.value = "";
+          clearSuggestions();
+        }
+      } else {
+        alert("No artists found for the given search term.");
+      }
+    } catch (error) {
+      console.error(`Error searching for artist:`, error);
+      alert("Failed to search for the artist. Please try again later.");
     }
   }
 };
-
-const searchButton = document.getElementById("searchButton");
-searchButton.addEventListener("click", searchArtist);
 
 const content = document.getElementById("content");
 
@@ -371,6 +397,51 @@ function updateHeartedArtists(artistId, hearted) {
 
   localStorage.setItem("heartedArtists", JSON.stringify(heartedArtists));
 }
+
+const resetArtistContainer = (artistId) => {
+  const artistContainer = content.querySelector(`[data-artist="${artistId}"]`);
+
+  const heartIcon = artistContainer.querySelector(".heart-icon");
+  heartIcon.innerHTML = isArtistHearted(artistId) ? "❤️" : "🤍";
+  heartIcon.classList.remove("pulsate");
+  clearAlbumDropdown();
+  clearTrackList(artistContainer.querySelector(".track-list-container"));
+};
+
+const toggleHeart = (artistId) => {
+  const artistContainer = content.querySelector(`[data-artist="${artistId}"]`);
+  const heartIcon = artistContainer.querySelector(".heart-icon");
+  const isLiked = isArtistHearted(artistId);
+
+  if (isLiked) {
+    updateHeartedArtists(artistId, false);
+    heartIcon.innerHTML = "🤍";
+    heartIcon.classList.remove("pulsate");
+  } else {
+    updateHeartedArtists(artistId, true);
+    heartIcon.innerHTML = "❤️";
+    heartIcon.classList.add("pulsate");
+  }
+};
+
+const clearAlbumDropdown = () => {
+  const albumDropdowns = content.querySelectorAll(
+    "select[id^='albumDropdown-']"
+  );
+  albumDropdowns.forEach((dropdown) => dropdown.remove());
+};
+
+const clearTrackList = (trackListContainer) => {
+  const existingTrackList = trackListContainer.querySelector("ul");
+  if (existingTrackList) {
+    existingTrackList.remove();
+  }
+
+  const existingAlbumCover = trackListContainer.querySelector(".album-cover");
+  if (existingAlbumCover) {
+    existingAlbumCover.remove();
+  }
+};
 
 const toggleLightMode = () => {
   document.body.classList.toggle('light-mode');
